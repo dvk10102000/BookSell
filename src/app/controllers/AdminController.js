d1b5c2119916d5d634290019bd03b0ee1084ef28
TrangTrang32 <phamthutrang03022000@gmail.com>
2021-08-16 20:35:07 +0700

    complete a part of items (delete and update)

--- src/app/controllers/AdminController.js
+++ src/app/controllers/AdminController.js
@@ -6,6 +6,26 @@ const CartNotLogin = require('../model/cart');
 // const books = require('../model/index');
 const { mutipleMongooseToObject } = require('../../util/mongoose');
 const { mongooseToObject } = require('../../util/mongoose');
+const multer  = require('multer');
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'src/public/img');
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now()  + "-" + file.originalname);
+    }
+});  
+let upload = multer({ 
+    storage: storage,
+    fileFilter: function (req, file, cb) {
+        console.log(file);
+        if(file.mimetype=="image/bmp" || file.mimetype=="image/png" || file.mimetype== "image/jpg" || file.mimetype== "image/jpeg"){
+            cb(null, true);
+        }else{
+            return cb(new Error('Only image are allowed!'));
+        }
+    }
+}).single("image");
 
 
 class HomeController {
@@ -26,6 +46,56 @@ class HomeController {
                 res.redirect('/');
             })
     }  
+    managerItems(req, res, next){
+       Books.find({})
+            .then(books =>{
+                res.render('managerItems',{
+                  book: mutipleMongooseToObject(books),
+                })
+            })
+    }
+
+    distroy(req, res, next){
+        Books.deleteOne({_id: req.params.id})
+             .then(() =>{
+                 res.redirect('back');
+             })
+    }
+    update(req, res, next){
+        Books.findOne({_id: req.params.id})
+             .then(book => {
+                 res.render('updateItems',mongooseToObject(book))
+             })
+    }
+    async updateItem(req, res, next){
+       
+       
+        let book = await Books.findOne({_id: req.params.id});
+                      
+        await upload(req, res, function (err) {
+            if (err instanceof multer.MulterError) {
+              console.log("A Multer error occurred when uploading."); 
+            } else if (err) {
+              console.log("An unknown error occurred when uploading." + err);
+            }else{
+                book.name = req.body.name;
+                book.priceCurrent = req.body.priceCurrent;
+                book.quantity = req.body.quantity;  
+                book.image = req.file.filename;
+                Books.updateOne({_id : req.params.id},book)
+                     .then(() => {
+                        res.redirect('/admin/managerItems');
+                     })
+                
+            }
+        })  
+
+            
+                        
+                     
+                        
+                
+    }
 }   
         
 module.exports = new HomeController;
