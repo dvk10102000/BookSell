4d74b302005be335dbec049e4316f2f73cdcdea9
khang dovan <dvk10102000@gmail.com>
2021-09-05 01:46:36 +0700

    change connect to database

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,8 +1,23 @@
 const mongoose = require('mongoose');
 
+// async function connect() {
+//     try {
+//         await mongoose.connect('mongodb://localhost:27017/BookStore', {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//             useFindAndModify: false,
+//             useCreateIndex: true
+//           });
+//           console.log('Connected successfully');
+//     }catch(err) {
+//             console.log('connect failure: ' + err);
+//     }
+// }
+
+
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/BookStore', {
+        await mongoose.connect('mongodb+srv://dvkhang:dovankhang123@bookstore.es6un.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
@@ -13,5 +28,4 @@ async function connect() {
             console.log('connect failure: ' + err);
     }
 }
-
 module.exports ={connect};
