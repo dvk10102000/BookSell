8eecbe4b4565c32fcb2ed3e5a9f9472324815926
khang dovan <dvk10102000@gmail.com>
2021-09-05 10:15:30 +0700

    new update

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-async function connect() {
-    try {
-        await mongoose.connect('mongodb://localhost:27017/BookStore', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true
-          });
-          console.log('Connected successfully');
-    }catch(err) {
-            console.log('connect failure: ' + err);
-    }
-}
+// async function connect() {
+//     try {
+//         await mongoose.connect('mongodb://localhost:27017/BookStore', {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//             useFindAndModify: false,
+//             useCreateIndex: true
+//           });
+//           console.log('Connected successfully');
+//     }catch(err) {
+//             console.log('connect failure: ' + err);
+//     }
+// }
 
 
 // async function connect() {
@@ -28,4 +28,13 @@ async function connect() {
 //             console.log('connect failure: ' + err);
 //     }
 // }
+
+const mongoose = require('mongoose');
+function connect() {
+    const connection = "mongodb+srv://dvkhang:dovankhang123@bookstore.es6un.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+        .then(() => console.log("Database Connected Successfully"))
+        .catch(err => console.log(err))
+}
+
 module.exports ={connect};
