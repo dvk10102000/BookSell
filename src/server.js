0962374356c3f9e5dd81d70e15e61123a3dc4f2d
khang dovan <dvk10102000@gmail.com>
2021-09-05 09:59:56 +0700

    new update

--- src/server.js
+++ src/server.js
@@ -112,7 +112,7 @@ io.on('connection', (socket) => {
 });
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 8000;
+  port = 3000;
 }
 server.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
